4ef0769757cd64ac06bd56af0455814968bdba45
Светлин Димитров <dimitrovmail99@gmail.com>
2024-07-29 13:40:52 +0300

    init repo

---
+++ src/test/java/com/intership/flow_appointment_scheduling/FlowAppointmentSchedulingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intership.flow_appointment_scheduling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowAppointmentSchedulingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
