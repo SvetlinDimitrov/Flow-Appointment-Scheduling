f51911dc9074ac43e83bfd850bfac571edb902ba
Светлин Димитров <dimitrovmail99@gmail.com>
2024-08-16 16:58:12 +0300

    add repository annotation for better readability

--- src/main/java/com/internship/flow_appointment_scheduling/features/service/repository/ServiceRepository.java
+++ src/main/java/com/internship/flow_appointment_scheduling/features/service/repository/ServiceRepository.java
@@ -4,7 +4,9 @@ import com.internship.flow_appointment_scheduling.features.service.entity.Servic
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ServiceRepository extends JpaRepository<Service, Long> {
 
   Page<Service> findAllByUsersEmail(String userEmail, Pageable pageable);
--- src/main/java/com/internship/flow_appointment_scheduling/features/service/repository/WorkSpaceRepository.java
+++ src/main/java/com/internship/flow_appointment_scheduling/features/service/repository/WorkSpaceRepository.java
@@ -3,7 +3,9 @@ package com.internship.flow_appointment_scheduling.features.service.repository;
 import com.internship.flow_appointment_scheduling.features.service.entity.WorkSpace;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface WorkSpaceRepository extends JpaRepository<WorkSpace, Long> {
 
   boolean existsByName(String name);
